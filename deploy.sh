b8b8dee6524eacf14c1dce8a5b56b73a51c67a6b
Xing Lin <xing@netapp.com>
2020-06-14 11:35:56 -0700

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,13 @@
 # If a command fails then the deploy stops
 set -e
 
-printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
+echo "commit the huge source repo"
+git add content/blog
+msg="update blog content $(date)"
+git commit -m "$msg"
+git push origin master
+
+printf "\033[0;32mDeploying hugo site to GitHub...\033[0m\n"
 
 # Build the project.
 hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
